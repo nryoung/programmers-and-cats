3974c5b53ca1adc8cfc7ecfc204adef3bf4513fe
Nic Young <nryoung@gmail.com>
2016-03-03 22:46:07 -0800

    Finally figured out how to get columns working correctly.
    - Sample repos are now rendering quite nicely
    - Updated Bulma so I could use the new offset features

--- app/components/SampleRepos.jsx
+++ app/components/SampleRepos.jsx
@@ -3,15 +3,28 @@ import React from 'react';
 class SampleRepos extends React.Component {
   render() {
     return (
-      <div className="sample-repos">
-        {this.props.repos.map(function(repo, i){
-          return (
-            <div key={i}>
-              <span><a href={repo.html_url}>{repo.full_name}</a> {repo.stargazers_count} Stargazers</span>
-              <br />
+      <div className="container">
+        <div className="columns is-mobile">
+          <div className="column is-third is-offset-4">
+            <div className="message is-primary">
+              <div className="message-header">
+                A small sampling of cat repos
+              </div>
+              <div className="message-body">
+                <div className="content">
+                {this.props.repos.map(function(repo, i){
+                  return (
+                    <p key={i}>
+                      <a href={repo.html_url}>{repo.full_name}</a> {repo.stargazers_count} Stargazers
+                      <br />
+                    </p>
+                  )
+                })}
+                </div>
+              </div>
             </div>
-          )
-        })}
+          </div>
+        </div>
       </div>
     )
   }
